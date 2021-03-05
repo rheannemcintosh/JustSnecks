295010811c8c4e2d8d010cffdd5727078a9b070f
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-03-05 23:46:49 +0000

    Create a food item and store it in the DB

--- app/Http/Controllers/FoodController.php
+++ app/Http/Controllers/FoodController.php
@@ -24,7 +24,7 @@ class FoodController extends Controller
      */
     public function create()
     {
-        //
+        return view('food.create');
     }
 
     /**
@@ -35,7 +35,29 @@ class FoodController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'name' => 'required',
+            'description' => 'required',
+            'price' => 'required|integer',
+            'category' => 'required',
+            'image' => 'required|mimes:png,jpeg,jpg',
+        ]);
+
+        $image           = $request->file('image');
+        $name            = time().'_'.$image->getClientOriginalExtension();
+        $destinationPath = public_path('\images');
+
+        $image->move($destinationPath, $name);
+
+        Food::create([
+            'name' => $request->get('name'),
+            'description' => $request->get('description'),
+            'price' => $request->get('price'),
+            'category_id' => $request->get('category'),
+            'image' => $name,
+        ]);
+
+        return redirect()->route('food.index')->with('message', 'Food Created');
     }
 
     /**
