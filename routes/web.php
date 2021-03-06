d6d7461a55765e5b8628df5cdb0c391adb3ae7f2
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-03-06 23:43:59 +0000

    Remove the home route

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,6 @@ use App\Http\Controllers\FoodController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
