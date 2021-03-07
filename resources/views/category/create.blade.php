b0e397769dbf050ec0896722ee7c708283b37238
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-03-07 16:16:07 +0000

    Format the category blade templates

--- resources/views/category/create.blade.php
+++ resources/views/category/create.blade.php
@@ -1,36 +1,39 @@
+<!-- category/create.blade.php | Rheanne McIntosh <rheanne.mcintosh@outlook.co.uk> | 07-03-2021 -->
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-
-            @if(Session::has('message'))
-                <div class="alert alert-success">
-                {{ Session::get('message')}}
-                </div>
-            @endif
-            <form action="{{ route('category.store') }}" method="post">@csrf
-            <div class="card">
-                <div class="card-header">Manage Food Category</div>
-
-                <div class="card-body">
-                    <div class="form-group">
-                        <label for="name">Name</label>
-                        <input type="text" name="name" class="form-control @error('name') is-invalid @enderror">
-                        @error('name')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                @if(Session::has('message'))
+                    <div class="alert alert-success">
+                        {{ Session::get('message') }}
                     </div>
-                    <div class="form-group">
-                        <button class="btn btn-outline-primary">Submit</button>
+                @endif
+
+                <form action="{{ route('category.store') }}" method="post">@csrf
+                    <div class="card">
+                        <div class="card-header">Manage Categories</div>
+
+                        <div class="card-body">
+                            <div class="form-group">
+                                <label for="name">Name</label>
+                                <input type="text" name="name" class="form-control @error('name') is-invalid @enderror">
+                                
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+
+                            <div class="form-group">
+                                <button class="btn btn-outline-primary">Submit</button>
+                            </div>
+                        </div>
                     </div>
-                </div>
+                </form>
             </div>
-            </form>
         </div>
     </div>
-</div>
 @endsection
--- resources/views/category/edit.blade.php
+++ resources/views/category/edit.blade.php
@@ -1,39 +1,40 @@
+<!-- category/edit.blade.php | Rheanne McIntosh <rheanne.mcintosh@outlook.co.uk> | 07-03-2021 -->
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-
-            @if(Session::has('message'))
-                <div class="alert alert-success">
-                {{ Session::get('message')}}
-                </div>
-            @endif
-
-            <form action="{{ route('category.update', [$category->id]) }}" method="post">@csrf
-            {{method_field('PUT')}}
-            <div class="card">
-                <div class="card-header">Update Food Category</div>
-
-                <div class="card-body">
-                    <div class="form-group">
-                        <label for="name">Name</label>
-                        <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" value="{{ $category->name }}">
-                        @error('name')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                @if(Session::has('message'))
+                    <div class="alert alert-success">
+                        {{ Session::get('message') }}
                     </div>
-                    <div class="form-group">
-                        <button class="btn btn-outline-primary">Update</button>
+                @endif
+
+                <form action="{{ route('category.update', [$category->id]) }}" method="post">@csrf
+                {{method_field('PUT')}}
+                    <div class="card">
+                        <div class="card-header">Update Food Category</div>
+
+                        <div class="card-body">
+                            <div class="form-group">
+                                <label for="name">Name</label>
+                                <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" value="{{ $category->name }}">
+                                
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+
+                            <div class="form-group">
+                                <button class="btn btn-outline-primary">Update</button>
+                            </div>
+                        </div>
                     </div>
-                </div>
+                </form> 
             </div>
-            </form>
-            
         </div>
     </div>
-</div>
 @endsection
