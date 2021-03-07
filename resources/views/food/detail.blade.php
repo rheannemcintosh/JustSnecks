be9f460d94e3b372bf1d71492c26a4d1d2b84c23
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-03-07 16:15:43 +0000

    Format the food blade templates

--- resources/views/food/detail.blade.php
+++ resources/views/food/detail.blade.php
@@ -1,31 +1,28 @@
+<!-- food/detail.blade.php | Rheanne McIntosh <rheanne.mcintosh@outlook.co.uk> | 07-03-2021 -->
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-4">
-            <div class="card">
-                <div class="card-header">{{ __('Product') }}</div>
-                
-                <div class="card-body">
-                    <img src="{{ asset('images')}}/{{$food->image}}" class="img-fluid">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-header">{{ __('Product') }}</div>
+                    <div class="card-body">
+                        <img src="{{ asset('images')}}/{{ $food->image }}" class="img-fluid">
+                    </div>
                 </div>
             </div>
 
-        </div>
-
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Detail') }}</div>
-
-                <div class="card-body">
-                    <h2>{{$food->name}}</h2>
-                    <p class="lead">{{$food->description}}</p>
-                    <p>£{{$food->price}}</p>
-                    
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Detail') }}</div>
+                    <div class="card-body">
+                        <h2>{{ $food->name }}</h2>
+                        <p class="lead">{{ $food->description }}</p>
+                        <p>£{{ $food->price }}</p>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
